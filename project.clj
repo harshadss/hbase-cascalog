54c6b1027f74d1d42115e3c8653f5cf303817943
Harshad Saykhedkar <harshad.say@gmail.com>
2014-04-11 12:21:22 +0530

    Added as-string helper function to ops. Bumped up cascalog version.

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :description "simple cascalog wrapper for cascading.hbase"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojars.sorenmacbeth/cascading.hbase "1.3.0-SNAPSHOT"]
-                 [cascalog/cascalog-core "2.0.0-SNAPSHOT"]
+                 [cascalog/cascalog-core "2.1.0"]
                  [org.apache.thrift/libthrift "0.8.0"
                   :exclusions
                   [org.slf4j/slf4j-api]]]
   :profiles {:provided
              {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2"]
-                             [org.apache.hbase/hbase "0.92.0"
+                             [org.apache.hbase/hbase "0.94.18"
                               :exclusions [org.apache.thrift/libthrift
                                            org.slf4j/slf4j-api]]]}
              :dev
--- src/clj/hbase/cascalog/ops.clj
+++ src/clj/hbase/cascalog/ops.clj
@@ -1,3 +1,12 @@
 (ns hbase.cascalog.ops
   (:use cascalog.api)
   (:import [cascading.hbase.helper HBaseMapToTuples HBaseTuplesToMap]))
+
+(defn as-string
+  "Used for de-serializing bytes coming out of HBase to String"
+  [bytes]
+  (if (nil? bytes)
+     ""
+     (String. bytes)
+  )
+)
